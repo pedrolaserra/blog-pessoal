cac72ce210d8edf3a97f5c659d03be5ae878c1dd
pedrolaserra <pedro10souza@hotmail.com>
2023-05-05 10:15:03 -0300

    sobremim texto alterado

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -21,7 +21,7 @@ export default function SobreMim() {
 <p className={styles.paragrafo}>Tudo bem?</p>
 
 
-<p className={styles.paragrafo}>Tenho 27 anos, cresci no interior Paulista, na cidade de Tupã, aos 18 anos após 1 ano todo de etapas a do concurso da PMESP, tomava posse desse cargo público tão sonhado por mim na época, cargo o qual estou até o presente momento.</p>
+<p className={styles.paragrafo}>Tenho 27 anos, cresci no interior Paulista, na cidade de Tupã, aos 18 anos após 1 ano todo de etapas do concurso da PMESP, tomava posse desse cargo público tão sonhado por mim na época, cargo o qual estou até o presente momento.</p>
 
 
 <p className={styles.paragrafo}>Todos esses anos na Polícia Militar de São Paulo fui notando o quanto segurança pública e tecnologia precisam andar de mãos dadas, sendo que por diversas vezes a prisão em flagrante ou até mesmo, de forma posterior, a condenação de criminosos só foi possível após a utilização de algum software, seja por radares inteligentes, aplicativos de pesquisa em banco de dados e etc.</p>
